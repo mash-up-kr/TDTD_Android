fd3d9b65891dba33f59d9ee9a6d8d3e6f8587b09
jsh-me <ppm_it@naver.com>
2021-02-20 01:14:14 +0900

    Add MainActivity

---
+++ presentation/src/main/java/com/tdtd/presentation/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tdtd.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tdtd.presentation.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
