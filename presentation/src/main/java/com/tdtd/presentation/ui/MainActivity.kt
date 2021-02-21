7d9a1af6b836ef049233e4e7143ecbf1d80344f2
jsh-me <ppm_it@naver.com>
2021-02-21 17:46:51 +0900

    Implement Main View

--- presentation/src/main/java/com/tdtd/presentation/ui/MainActivity.kt
+++ presentation/src/main/java/com/tdtd/presentation/ui/MainActivity.kt
@@ -8,8 +8,9 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
