07d9a13b75884e20bf0d190b84c2bca5d7daadcc
d2fault <d2fault@naver.com>
2021-03-07 01:08:15 +0900

    Add Favorites Button Click Event
    - 즐겨찾기 버튼 클릭시 icon 이미지가 변경됩니다.
    - findViewById 대신 View Binding을 사용하도록 수정합니다.
    - 서버 데이터에 따른 최초 버튼 이미지 세팅이 필요합니다.

--- presentation/src/main/java/com/tdtd/presentation/ui/detail/DetailAdminActivity.kt
+++ presentation/src/main/java/com/tdtd/presentation/ui/detail/DetailAdminActivity.kt
@@ -3,36 +3,41 @@ package com.tdtd.presentation.ui.detail
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.FrameLayout
-import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
+import androidx.databinding.DataBindingUtil
 import com.tdtd.presentation.R
-import com.tdtd.presentation.databinding.ActivityMainBinding
+import com.tdtd.presentation.databinding.ActivityDetailAdminBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class DetailAdminActivity : AppCompatActivity() {
 
-    private lateinit var toolbar: Toolbar
-    private lateinit var actionBar: ActionBar
-    private lateinit var view: View
-    private lateinit var frameLayout: FrameLayout
     private lateinit var inflater: LayoutInflater
+    private lateinit var view: View
+
+    private lateinit var binding: ActivityDetailAdminBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_admin)
 
+        initView()
+        onClickFavoritesButton()
+    }
+
+    private fun initView() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_detail_admin)
+        binding.lifecycleOwner = this
+
         inflater = getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        frameLayout = findViewById<View>(R.id.detailAdminFrameLayout) as FrameLayout
 
-        view = inflater.inflate(R.layout.layout_detail_admin, frameLayout, false)
-        frameLayout.addView(view)
+        view = inflater.inflate(R.layout.layout_detail_admin, binding.detailAdminFrameLayout, false)
+        binding.detailAdminFrameLayout.addView(view)
+    }
 
-        toolbar = findViewById(R.id.app_toolbar)
-        setSupportActionBar(toolbar)
-        actionBar = supportActionBar!!
-        actionBar.setDisplayShowCustomEnabled(true)
+    private fun onClickFavoritesButton() {
+        binding.favoritesButton.setOnClickListener {
+            binding.favoritesButton.isSelected = !binding.favoritesButton.isSelected
+        }
     }
 }
