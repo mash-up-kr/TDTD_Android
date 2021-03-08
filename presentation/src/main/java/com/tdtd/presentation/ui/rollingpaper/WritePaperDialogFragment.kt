1fb3dd45f3fa04b603812e7bac065c36b043cc90
d2fault <d2fault@naver.com>
2021-03-09 02:53:00 +0900

    Add Write Paper Bottom Sheet
    - 롤링페이퍼 작성(텍스트)을 위한 Bottom Sheet를 추가합니다.

---
+++ presentation/src/main/java/com/tdtd/presentation/ui/rollingpaper/WritePaperDialogFragment.kt
@@ -0,0 +1,83 @@
+package com.tdtd.presentation.ui.rollingpaper
+
+import android.app.Dialog
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.DisplayMetrics
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.tdtd.presentation.R
+import com.tdtd.presentation.util.toPx
+import kotlinx.android.synthetic.main.bottom_sheet_write_paper.*
+
+class WritePaperDialogFragment : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.DialogStyle)
+        return inflater.inflate(R.layout.bottom_sheet_write_paper, container, false)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return BottomSheetDialog(requireContext(), theme).apply {
+            behavior.state = BottomSheetBehavior.STATE_EXPANDED
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initParentHeight()
+        setNicknameEditFocus()
+        setNicknameEditView()
+        setRollingPager()
+    }
+
+    private fun setNicknameEditFocus() {
+        nicknameEditText.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) nicknameEditText.setBackgroundResource(R.drawable.background_beige2_stroke1_gray2_radius16)
+            else nicknameEditText.setBackgroundResource(R.drawable.background_beige2_stroke1_beige3_radius16)
+        }
+    }
+
+    private fun setNicknameEditView() {
+        currentTextLengthTextView.text = getString(R.string.recode_paper_nickname_number, 0)
+
+        nicknameEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                nicknameEditText.setBackgroundResource(R.drawable.background_beige2_stroke1_gray2_radius16)
+                currentTextLengthTextView.text = getString(R.string.recode_paper_nickname_number, s?.length)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        })
+    }
+
+    private fun setRollingPager() {
+    }
+
+    private fun initParentHeight() {
+        val displayMetrics = DisplayMetrics()
+        requireActivity().windowManager.defaultDisplay.getMetrics(displayMetrics)
+
+        val deviceHeight: Int = displayMetrics.heightPixels
+        val layoutParams = view?.layoutParams
+        layoutParams?.height = deviceHeight - 24.toPx()
+
+        view?.layoutParams = layoutParams
+    }
+}
