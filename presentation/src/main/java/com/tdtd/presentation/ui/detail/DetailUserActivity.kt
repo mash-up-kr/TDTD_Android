495e05c479d3d088e643757e0396affc2427a9b9
d2fault <d2fault@naver.com>
2021-04-12 02:03:39 +0900

    test: 디테일 페이지에서 리스트를 확인할 수 있는 테스트 코드를 추가합니다.
    - 필수적으로 수정해야 합니다.
    - 클릭 이벤트에 따라 이미지 변화를 보려고 넣은 코드에 문제(스크롤하며 터치하면 앱이 죽습니다.)가 있습니다.

--- presentation/src/main/java/com/tdtd/presentation/ui/detail/DetailUserActivity.kt
+++ presentation/src/main/java/com/tdtd/presentation/ui/detail/DetailUserActivity.kt
@@ -5,13 +5,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.get
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
+import androidx.recyclerview.widget.GridLayoutManager
 import com.tdtd.presentation.R
 import com.tdtd.presentation.databinding.ActivityDetailUserBinding
+import com.tdtd.presentation.entity.RoomContents
+import com.tdtd.presentation.entity.getDefaultCharacter
+import com.tdtd.presentation.entity.getRoomContents
+import com.tdtd.presentation.entity.getSelectedCharacter
 import com.tdtd.presentation.ui.dialog.DialogConstructor
 import com.tdtd.presentation.ui.writetext.WriteTextDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.layout_detail_room_contents.*
+import kotlinx.android.synthetic.main.row_detail_items.view.*
 
 @AndroidEntryPoint
 class DetailUserActivity : AppCompatActivity() {
@@ -20,6 +28,28 @@ class DetailUserActivity : AppCompatActivity() {
     private lateinit var view: View
 
     private lateinit var binding: ActivityDetailUserBinding
+    private val contentList: List<RoomContents> = getRoomContents()
+
+    companion object {
+        var currentPosition: Int = 0
+        var prevPosition: Int = 0
+    }
+
+    private var detailAdapter = DetailAdapter() { position ->
+        // TODO: companion object를 포함하여 테스트 용으로 추가한 코드라 수정해야 합니다. 스크롤하면서 터치하면 앱 죽어요.....
+        prevPosition = currentPosition
+        currentPosition = position
+        recyclerView.get(prevPosition).characterImageView.setImageResource(
+            getDefaultCharacter(
+                contentList.get(prevPosition).sticker_color
+            )
+        )
+        recyclerView.get(currentPosition).characterImageView.setImageResource(
+            getSelectedCharacter(
+                contentList.get(currentPosition).sticker_color
+            )
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,8 +68,19 @@ class DetailUserActivity : AppCompatActivity() {
 
         inflater = getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
 
-        view = inflater.inflate(R.layout.layout_detail_user, binding.detailUserFrameLayout, false)
+        // TODO: contents 유무에 따라 view가 달라져야 합니다.
+        view = inflater.inflate(
+            R.layout.layout_detail_room_contents,
+            binding.detailUserFrameLayout,
+            false
+        )
+//        view = inflater.inflate(R.layout.layout_detail_user, binding.detailUserFrameLayout, false)
         binding.detailUserFrameLayout.addView(view)
+
+        recyclerView.layoutManager = GridLayoutManager(this, 3)
+        detailAdapter.submitList(contentList)
+        recyclerView.adapter = detailAdapter
+        recyclerView.adapter?.notifyDataSetChanged()
     }
 
     private fun onClickFavoritesButton() {
