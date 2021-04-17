b498a929da63c23958cdebc1a8dcef4c1e690681
jsh-me <ppm_it@naver.com>
2021-04-17 23:54:48 +0900

    Add Inject Annotation In Usecase

--- domain/src/main/java/com/tdtd/domain/usecase/GetAllRoomsUseCase.kt
+++ domain/src/main/java/com/tdtd/domain/usecase/GetAllRoomsUseCase.kt
@@ -3,8 +3,9 @@ package com.tdtd.domain.usecase
 import com.tdtd.domain.repository.RoomRepository
 import com.tdtd.domain.Result
 import com.tdtd.domain.entity.RoomsEntity
+import javax.inject.Inject
 
-class GetAllRoomsUseCase(
+class GetAllRoomsUseCase @Inject constructor(
     private val roomRepository: RoomRepository
 ) {
     suspend operator fun invoke(): Result<List<RoomsEntity>> =
