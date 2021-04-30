5c6bf1b818ae53a7e0f8cbb8cd844db07e8d7100
SeungYooon <tmddbs4998@naver.com>
2021-04-30 22:15:46 +0900

    fix: ApplicationComponent to SingletonComponent

--- app/src/main/java/com/tdtd/voicepaper/di/module/DispatcherModule.kt
+++ app/src/main/java/com/tdtd/voicepaper/di/module/DispatcherModule.kt
@@ -6,12 +6,12 @@ import com.tdtd.domain.MainDispatcher
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 
 @Module
-@InstallIn(ApplicationComponent::class)
+@InstallIn(SingletonComponent::class)
 object DispatcherModule {
     @DefaultDispatcher
     @Provides
--- app/src/main/java/com/tdtd/voicepaper/di/module/UseCaseModule.kt
+++ app/src/main/java/com/tdtd/voicepaper/di/module/UseCaseModule.kt
@@ -1,20 +1,30 @@
 package com.tdtd.voicepaper.di.module
 
+import com.tdtd.domain.repository.BookmarkRepository
 import com.tdtd.domain.repository.RoomRepository
+import com.tdtd.domain.usecase.GetAllBookmarksUseCase
 import com.tdtd.domain.usecase.GetAllRoomsUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.components.ViewModelComponent
+
 //hilt version upgrade 하면서 ViewModelScope로 변경할 것
 @Module
-@InstallIn(ApplicationComponent::class)
+@InstallIn(ViewModelComponent::class)
 object UseCaseModule {
 
     @Provides
     fun provideGetAllRoomsUseCase(
         repository: RoomRepository,
     ): GetAllRoomsUseCase = GetAllRoomsUseCase(
+        repository
+    )
+
+    @Provides
+    fun provideGetAllBookmarksUseCase(
+        repository: BookmarkRepository
+    ): GetAllBookmarksUseCase = GetAllBookmarksUseCase(
         repository
     )
 }
