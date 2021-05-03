96014c4cc94e3416f64ef6ed4732e97b7e2e16c3
SeungYooon <tmddbs4998@naver.com>
2021-05-04 01:19:02 +0900

    feat: Bookmark 네트워크 설정

--- domain/src/main/java/com/tdtd/domain/usecase/GetAllBookmarksUseCase.kt
+++ domain/src/main/java/com/tdtd/domain/usecase/GetAllBookmarksUseCase.kt
@@ -1,7 +1,9 @@
 package com.tdtd.domain.usecase
 
 import com.tdtd.domain.Result
+import com.tdtd.domain.entity.DeleteRoomEntity
 import com.tdtd.domain.entity.RoomEntity
+import com.tdtd.domain.entity.RoomsEntity
 import com.tdtd.domain.repository.BookmarkRepository
 import javax.inject.Inject
 
@@ -10,4 +12,10 @@ class GetAllBookmarksUseCase @Inject constructor(
 ) {
     suspend operator fun invoke(): Result<List<RoomEntity>> =
         bookmarkRepository.getUserBookmarkList()
+
+    suspend fun postBookmark(roomCode:String) : Result<DeleteRoomEntity> =
+        bookmarkRepository.postBookmarkByRoomCode(roomCode)
+
+    suspend fun deleteBookmark(roomCode: String) :Result<DeleteRoomEntity> =
+        bookmarkRepository.deleteBookmarkByRoomCode(roomCode)
 }
