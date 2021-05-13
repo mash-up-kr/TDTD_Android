efb7e3f862cd99ba72d30f331bab9e604f5cfa3b
SeungYooon <tmddbs4998@naver.com>
2021-05-13 20:17:51 +0900

    feat: 음성 코멘트 구현

---
+++ presentation/src/main/java/com/tdtd/presentation/ui/detail/DetailFragment.kt
@@ -0,0 +1,457 @@
+package com.tdtd.presentation.ui.detail
+
+import android.media.MediaPlayer
+import android.os.Handler
+import android.os.Looper
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.tdtd.domain.entity.MakeRoomType
+import com.tdtd.presentation.R
+import com.tdtd.presentation.base.ui.BaseFragment
+import com.tdtd.presentation.databinding.FragmentDetailBinding
+import com.tdtd.presentation.ui.main.MainViewModel
+import com.tdtd.presentation.util.*
+import com.tdtd.presentation.util.Constants.STATE_PAUSE
+import com.tdtd.presentation.util.Constants.STATE_PLAYING
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_text_comment.*
+import kotlinx.android.synthetic.main.fragment_voice_comment.*
+import kotlinx.coroutines.*
+
+
+@AndroidEntryPoint
+class DetailFragment : BaseFragment<FragmentDetailBinding>(R.layout.fragment_detail) {
+
+    private val detailViewModel: DetailViewModel by viewModels()
+    private val safeArgs: DetailFragmentArgs by navArgs()
+    private lateinit var detailAdapter: DetailAdapter
+    private var type: String = ""
+    private val mainViewModel: MainViewModel by viewModels()
+    private var isFavorite: Boolean = false
+    private var endTime = 0
+    private var currentState = STATE_PLAYING
+    private var isPlaying = false
+    private var mediaPlayer: MediaPlayer? = null
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<*>
+    private val dispatcher = PauseDispatcher(Handler(Looper.getMainLooper()))
+    private var job: Job? = null
+    private var currentProgress = 0
+
+    override fun initViews() {
+        super.initViews()
+
+        initBindings()
+        setAdapter()
+        onClickFavoritesButton()
+        onClickBackButton()
+        onClickSharedButton()
+        onClickMoreButton()
+        onClickLeaveRoomButton()
+    }
+
+    override fun initObserves() {
+        super.initObserves()
+
+        if (safeArgs.host)
+            hostDetailFragment()
+        else
+            userDetailFragment()
+
+        detailViewModel.alreadyReportEvent.observe(viewLifecycleOwner, Observer {
+            requireActivity().showToast(
+                getString(R.string.dialog_report_reduplicate),
+                requireView()
+            )
+        })
+
+        detailViewModel.notMineEvent.observe(viewLifecycleOwner, Observer {
+            requireActivity().showToast(getString(R.string.dialog_delete_not_mine), requireView())
+        })
+
+        detailViewModel.detailRoom.observe(viewLifecycleOwner, Observer { detailRoom ->
+            binding.titleTextView.text = detailRoom.result.title
+
+            if (detailRoom.result.comments.isEmpty() && safeArgs.host) {
+                hideDetailRecyclerView()
+            } else if (detailRoom.result.comments.isEmpty() && !safeArgs.host) {
+                hideEmptyDetailRoom()
+            } else {
+                hideNoReplyTextView()
+                hideEmptyDetailRoom()
+                showDetailRecyclerView()
+            }
+
+            type = when (detailRoom.result.type) {
+                MakeRoomType.TEXT -> {
+                    startWriteTextDetailFragment()
+                    "text"
+                }
+                MakeRoomType.VOICE -> {
+                    startRecordVoiceDialogFragment()
+                    "voice"
+                }
+            }
+        })
+
+        detailViewModel.getRoomDetailByRoomCode(safeArgs.roomCode)
+    }
+
+    private fun initBindings() {
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = detailViewModel
+        isFavorite = safeArgs.bookmark
+    }
+
+    private fun setAdapter() {
+        detailAdapter = DetailAdapter { comments ->
+            if (comments.text != null) {
+                showTextCommentBottomSheet(
+                    comments.nickname,
+                    comments.text,
+                    comments.id,
+                    comments.is_mine
+                )
+            } else if (comments.text == null) {
+                showVoiceCommentBottomSheet(
+                    comments.nickname,
+                    comments.id,
+                    comments.is_mine,
+                    comments.voice_file_url
+                )
+            }
+        }
+        binding.detailRecyclerView.adapter = detailAdapter
+    }
+
+    private fun showTextCommentBottomSheet(
+        name: String,
+        content: String?,
+        id: Long?,
+        mine: Boolean
+    ) {
+        bottomSheetBehavior = BottomSheetBehavior.from(textCommentBottomSheet).apply {
+            this.state = BottomSheetBehavior.STATE_EXPANDED
+
+            val nickName = requireActivity().findViewById<TextView>(R.id.nicknameTextView)
+            val contents = requireActivity().findViewById<TextView>(R.id.contentsTextView)
+            val closeImageView = requireActivity().findViewById<ImageView>(R.id.closeImageView)
+            val report = requireActivity().findViewById<ImageView>(R.id.reportImageView)
+            val remove = requireActivity().findViewById<ImageView>(R.id.removeImageView)
+
+            nickName.text = name
+            contents.text = content
+
+            closeImageView.setOnClickListener {
+                bottomSheetBehavior.state =
+                    BottomSheetBehavior.STATE_HIDDEN
+            }
+
+            report.setOnClickListener {
+                if (mine) requireActivity().showToast(
+                    getString(R.string.dialog_report_mine),
+                    requireView()
+                )
+                else showReportCommentDialog(id!!)
+            }
+            remove.setOnClickListener {
+                if (safeArgs.host) showDeleteByHostCommentDialog(id!!)
+                else showDeleteCommentDialog(id!!)
+            }
+        }
+    }
+
+    private fun showVoiceCommentBottomSheet(
+        name: String,
+        id: Long?,
+        mine: Boolean,
+        voiceFileUrl: String?
+    ) {
+        bottomSheetBehavior = BottomSheetBehavior.from(voiceCommentBottomSheet).apply {
+            this.state = BottomSheetBehavior.STATE_EXPANDED
+
+            val nickName = requireActivity().findViewById<TextView>(R.id.nicknameVoiceTextView)
+            val closeImageView = requireActivity().findViewById<ImageView>(R.id.closeVoiceImageView)
+            val report = requireActivity().findViewById<ImageView>(R.id.reportVoiceImageView)
+            val remove = requireActivity().findViewById<ImageView>(R.id.removeVoiceImageView)
+            val recordImageView =
+                requireActivity().findViewById<ImageView>(R.id.recordDefaultImageView)
+            val endTimeTextView = requireActivity().findViewById<TextView>(R.id.endTimeTextView)
+            val progressBar = requireActivity().findViewById<ProgressBar>(R.id.progressBar)
+
+            progressBar.progress = 0
+
+            nickName.text = name
+
+            closeImageView.setOnClickListener {
+                bottomSheetBehavior.state =
+                    BottomSheetBehavior.STATE_HIDDEN
+            }
+
+            report.setOnClickListener {
+                if (mine) requireActivity().showToast(
+                    getString(R.string.dialog_report_mine),
+                    requireView()
+                )
+                else showReportCommentDialog(id!!)
+            }
+            remove.setOnClickListener {
+                if (safeArgs.host) showDeleteByHostCommentDialog(id!!)
+                else showDeleteCommentDialog(id!!)
+            }
+
+            mediaPlayer = MediaPlayer()
+
+            job?.cancel()
+            MediaPlayerHelper.stopAndRelease()
+            progressBar.progress = 0
+            recordImageView.setImageResource(R.drawable.ic_speak_play_32)
+            currentState = STATE_PLAYING
+
+            mediaPlayer.let { mediaPlayer ->
+                mediaPlayer?.setDataSource(voiceFileUrl)
+                mediaPlayer?.prepare()
+                currentProgress = mediaPlayer!!.currentPosition
+            }
+
+            if (mediaPlayer!!.duration.toLong() > 60000) endTimeTextView.text = "01:00"
+            else playerFormat(mediaPlayer?.duration!!.toLong(), endTimeTextView)
+
+            endTime = mediaPlayer!!.duration / 100
+            progressBar.max = endTime
+
+            recordImageView.setOnClickListener {
+                CoroutineScope(Dispatchers.Main).launch {
+                    changeByState(voiceFileUrl, recordImageView, progressBar)
+                }
+            }
+        }
+    }
+
+    private suspend fun changeByState(
+        url: String?,
+        imageView: ImageView,
+        progressBar: ProgressBar
+    ) {
+        val handler = Handler(Looper.getMainLooper())
+
+        when (currentState) {
+            STATE_PLAYING -> {
+                isPlaying = true
+                currentState = STATE_PAUSE
+                imageView.setImageResource(R.drawable.ic_speak_stop_32)
+
+                job = GlobalScope.launch(dispatcher) {
+                    if (this.isActive) {
+                        while (currentProgress < endTime) {
+                            currentProgress++
+                            try {
+                                delay(100)
+                            } finally {
+                                handler.post { progressBar.progress = currentProgress }
+                            }
+                        }
+                    }
+                }
+                dispatcher.resume()
+
+                MediaPlayerHelper.startPlaying(url!!) {
+                    isPlaying = false
+                    currentState = STATE_PLAYING
+                    imageView.setImageResource(R.drawable.ic_speak_play_32)
+                    job?.cancel()
+                }
+            }
+            STATE_PAUSE -> {
+                MediaPlayerHelper.pausePlaying()
+                isPlaying = false
+                currentState = STATE_PLAYING
+                imageView.setImageResource(R.drawable.ic_speak_play_32)
+                dispatcher.pause()
+                job?.cancelAndJoin()
+            }
+        }
+    }
+
+    private fun showDeleteCommentDialog(id: Long) {
+        val action =
+            DetailFragmentDirections.actionDetailFragmentToCustomDialogFragment(
+                "",
+                id,
+                R.layout.dialog_delete_reply,
+                isFavorite,
+                false
+            )
+        findNavController().navigate(action)
+    }
+
+    private fun showDeleteByHostCommentDialog(id: Long) {
+        val action =
+            DetailFragmentDirections.actionDetailFragmentToCustomDialogFragment(
+                "",
+                id,
+                R.layout.dialog_delete_reply_admin,
+                isFavorite,
+                true
+            )
+        findNavController().navigate(action)
+    }
+
+    private fun showReportCommentDialog(id: Long) {
+        val action =
+            DetailFragmentDirections.actionDetailFragmentToCustomDialogFragment(
+                "",
+                id,
+                R.layout.dialog_report_reply,
+                isFavorite,
+                safeArgs.host
+            )
+        findNavController().navigate(action)
+    }
+
+    private fun startRecordVoiceDialogFragment() {
+        binding.writeButton.setOnClickListener {
+            val action =
+                DetailFragmentDirections.actionDetailFragmentToRecordVoiceDialogFragment(
+                    safeArgs.roomCode, safeArgs.host, isFavorite
+                )
+            findNavController().navigate(action)
+        }
+    }
+
+    private fun startWriteTextDetailFragment() {
+        binding.writeButton.setOnClickListener {
+            val action =
+                DetailFragmentDirections.actionDetailFragmentToWriteTextDialogFragment(
+                    safeArgs.roomCode, safeArgs.host, isFavorite
+                )
+            findNavController().navigate(action)
+        }
+    }
+
+    private fun onClickMoreButton() {
+        binding.moreButton.setOnClickListener {
+            val action =
+                DetailFragmentDirections.actionDetailFragmentToDetailSharedBottomSheetFragment(
+                    safeArgs.roomCode,
+                    safeArgs.createdAt,
+                    isFavorite
+                )
+            findNavController().navigate(action)
+        }
+    }
+
+    private fun onClickSharedButton() {
+        binding.sharedLinkButton.setOnClickListener {
+            val action =
+                DetailFragmentDirections.actionDetailFragmentToDetailSharedBottomSheetFragment(
+                    safeArgs.roomCode,
+                    safeArgs.createdAt,
+                    isFavorite
+                )
+            findNavController().navigate(action)
+            showNoReplyTextView()
+        }
+    }
+
+    private fun onClickFavoritesButton() {
+        binding.favoritesButton.isSelected = isFavorite
+
+        binding.favoritesButton.setOnClickListener {
+            when (isFavorite) {
+                true -> {
+                    binding.favoritesButton.isSelected = false
+                    isFavorite = false
+                    PreferenceManager(requireContext()).setFavorite(false)
+                    mainViewModel.deleteBookmarkByRoomCode(safeArgs.roomCode)
+                }
+                false -> {
+                    binding.favoritesButton.isSelected = true
+                    isFavorite = true
+                    PreferenceManager(requireContext()).setFavorite(true)
+                    mainViewModel.postBookmarkByRoomCode(safeArgs.roomCode)
+                }
+            }
+        }
+    }
+
+    private fun onClickBackButton() {
+        binding.backButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+    private fun onClickLeaveRoomButton() {
+        binding.leaveRoomButton.setOnClickListener {
+            val action =
+                DetailFragmentDirections.actionDetailFragmentToCustomDialogFragment(
+                    safeArgs.roomCode,
+                    0,
+                    R.layout.dialog_leave_room,
+                    isFavorite,
+                    safeArgs.host
+                )
+            findNavController().navigate(action)
+        }
+    }
+
+    private fun hostDetailFragment() {
+        binding.moreButton.isVisible = true
+        binding.leaveRoomButton.isVisible = false
+        binding.sharedLinkButton.isVisible = true
+        binding.sharedLinkTextView.isVisible = true
+        binding.sharedFriendTextView.isVisible = false
+    }
+
+    private fun userDetailFragment() {
+        binding.moreButton.isVisible = false
+        binding.leaveRoomButton.isVisible = true
+        binding.sharedLinkButton.isVisible = false
+        binding.sharedLinkTextView.isVisible = false
+        binding.sharedFriendTextView.isVisible = true
+    }
+
+    private fun showEmptyDetailRoom() {
+        binding.sharedLinkTextView.isVisible = true
+    }
+
+    private fun hideEmptyDetailRoom() {
+        binding.sharedLinkTextView.isVisible = false
+    }
+
+    private fun showNoReplyTextView() {
+        binding.sharedLinkButton.isVisible = false
+        binding.sharedLinkTextView.isVisible = false
+        binding.sharedNoReplyTextView.isVisible = true
+    }
+
+    private fun hideNoReplyTextView() {
+        binding.sharedLinkButton.isVisible = false
+        binding.sharedLinkTextView.isVisible = false
+        binding.sharedNoReplyTextView.isVisible = false
+        binding.sharedFriendTextView.isVisible = false
+    }
+
+    private fun showDetailRecyclerView() {
+        binding.detailRecyclerView.isVisible = true
+    }
+
+    private fun hideDetailRecyclerView() {
+        binding.detailRecyclerView.isVisible = false
+    }
+
+    override fun onDestroy() {
+        if (isPlaying) {
+            mediaPlayer?.stop()
+            mediaPlayer?.release()
+            mediaPlayer = null
+        }
+        super.onDestroy()
+    }
+}
--- presentation/src/main/java/com/tdtd/presentation/ui/reply/RecordVoiceDialogFragment.kt
+++ presentation/src/main/java/com/tdtd/presentation/ui/reply/RecordVoiceDialogFragment.kt
@@ -3,6 +3,7 @@ package com.tdtd.presentation.ui.reply
 import android.Manifest
 import android.app.Dialog
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.os.SystemClock
 import android.text.Editable
@@ -13,9 +14,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.app.ActivityCompat
 import androidx.core.content.res.ResourcesCompat
+import androidx.core.net.toUri
 import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
@@ -33,15 +36,14 @@ import com.tdtd.presentation.util.Constants.STATE_RECORD
 import com.tdtd.presentation.util.Constants.STATE_RECORD_STOP
 import com.tdtd.presentation.util.MultiPartForm.getAudioBody
 import com.tdtd.presentation.util.MultiPartForm.getBody
-import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.MultipartBody
-import okhttp3.RequestBody.Companion.toRequestBody
 import java.io.File
+
 
 class RecordVoiceDialogFragment : BottomSheetDialogFragment() {
 
     private lateinit var binding: FragmentRecordVoiceBinding
-    private val detailViewModel: DetailViewModel by viewModels({ requireParentFragment() })
+    private val detailViewModel: DetailViewModel by activityViewModels()
     private val safeArgs: RecordVoiceDialogFragmentArgs by navArgs()
     private var file: String = ""
     private var currentState = STATE_NORMAL
@@ -50,6 +52,7 @@ class RecordVoiceDialogFragment : BottomSheetDialogFragment() {
     private var permissionToRecordAccepted = false
     private var permissions: Array<String> = arrayOf(Manifest.permission.RECORD_AUDIO)
     private var nickNameText = ""
+    private lateinit var keyboardVisibilityUtils: KeyboardVisibilityUtils
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -63,26 +66,25 @@ class RecordVoiceDialogFragment : BottomSheetDialogFragment() {
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return BottomSheetDialog(requireContext(), theme).apply {
-            behavior.state = BottomSheetBehavior.STATE_EXPANDED
-            behavior.peekHeight = 0
-            behavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
-                override fun onStateChanged(bottomSheet: View, newState: Int) {
-                    if (newState == BottomSheetBehavior.STATE_COLLAPSED) {
-                        behavior.state = BottomSheetBehavior.STATE_HIDDEN
-                    }
-                }
-
-                override fun onSlide(bottomSheet: View, slideOffset: Float) {
-                }
-            })
+        val dialog = BottomSheetDialog(requireContext(), theme)
+        dialog.setOnShowListener {
+            val bottomSheetDialog = it as BottomSheetDialog
+            val parentLayout =
+                bottomSheetDialog.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
+            parentLayout?.let { sheet ->
+                val behavior = BottomSheetBehavior.from(sheet)
+                setupFullHeight(sheet)
+                behavior.state = BottomSheetBehavior.STATE_EXPANDED
+                behavior.skipCollapsed = true
+            }
         }
+        return dialog
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        initParentHeight(requireActivity(), view, 24)
+        observeKeyboard()
         setBottomSheetPadding(view)
         initRecord()
         onClickCancelButton()
@@ -93,6 +95,29 @@ class RecordVoiceDialogFragment : BottomSheetDialogFragment() {
     override fun onResume() {
         super.onResume()
         onClickRecord()
+    }
+
+    private fun observeKeyboard() {
+        keyboardVisibilityUtils = KeyboardVisibilityUtils(requireActivity().window,
+            onShowKeyboard = { _, _ ->
+                hideBanner()
+            },
+            onHideKeyboard = {
+                showBanner()
+            }
+        )
+    }
+
+    private fun showBanner() {
+        binding.bannerView.isVisible = true
+        binding.bannerImageView.isVisible = true
+        binding.bannerTextView.isVisible = true
+    }
+
+    private fun hideBanner() {
+        binding.bannerView.isVisible = false
+        binding.bannerImageView.isVisible = false
+        binding.bannerTextView.isVisible = false
     }
 
     private fun setBottomSheetPadding(view: View) {
@@ -107,7 +132,7 @@ class RecordVoiceDialogFragment : BottomSheetDialogFragment() {
     }
 
     private fun initRecord() {
-        file = "${requireActivity().externalCacheDir?.absolutePath}/recording.3gp"
+        file = "${requireActivity().externalCacheDir?.absolutePath}/recording.mp3"
         MediaRecorderHelper.fileName = file
 
         ActivityCompat.requestPermissions(
@@ -151,7 +176,12 @@ class RecordVoiceDialogFragment : BottomSheetDialogFragment() {
     }
 
     private fun onClickRecord() {
+        binding.view.setOnClickListener {
+            it.hideKeyboard()
+        }
+
         binding.recordDefaultImageView.setOnClickListener {
+            it.hideKeyboard()
             changeRecordState()
         }
     }
@@ -201,7 +231,6 @@ class RecordVoiceDialogFragment : BottomSheetDialogFragment() {
                 MediaRecorderHelper.stopAndRelease()
                 showRecordStop()
                 showReRecordPlay()
-
                 Log.v(TAG, "STATE_RECORD_STOP isRecord:$isRecord isPlaying:$isPlaying")
             }
             STATE_PLAYING -> {
@@ -241,7 +270,7 @@ class RecordVoiceDialogFragment : BottomSheetDialogFragment() {
             chronometer.base = SystemClock.elapsedRealtime()
             chronometer.start()
             chronometer.setOnChronometerTickListener {
-                if (it.text == getString(R.string.record_voice_maximum_minute)) {
+                if (it.text == getString(R.string.record_voice_maximum_minute) && nickNameText.isNotEmpty()) {
                     it.stop()
                     recordDefaultImageView.setImageResource(R.drawable.ic_icon_record_play)
                     recordStatusTextView.setText(R.string.record_voice_listen)
@@ -250,6 +279,7 @@ class RecordVoiceDialogFragment : BottomSheetDialogFragment() {
                     currentState = STATE_RECORD_STOP
                     MediaRecorderHelper.stopAndRelease()
                     showReRecordPlay()
+                    onClickCompleteButton()
                 }
             }
         }
@@ -346,16 +376,10 @@ class RecordVoiceDialogFragment : BottomSheetDialogFragment() {
     }
 
     private fun onClickCompleteButton() {
-        val voiceFile = File(file)
-        val voiceBody = voiceFile.absolutePath.toRequestBody("audio/*".toMediaType())
-        val fileToUpload = MultipartBody.Part.createFormData(
-            "voice_file",
-            voiceFile.absolutePath,
-            voiceBody
-        )
+        val audioPath: Uri = file.toUri()
+        val voiceFile = File(audioPath.path!!)
 
         val part: ArrayList<MultipartBody.Part> = ArrayList()
-
         part.add(getBody("nickname", nickNameText))
         part.add(getBody("message_type", "VOICE"))
         part.add(getAudioBody("voice_file", voiceFile))
@@ -371,11 +395,35 @@ class RecordVoiceDialogFragment : BottomSheetDialogFragment() {
                     safeArgs.roomCode,
                     part
                 ).also {
+                    if (safeArgs.host) createCommentByAdmin()
+                    else createComment()
                     dismiss()
                 }
             }
         }
     }
+
+    private fun createCommentByAdmin() {
+        val action =
+            RecordVoiceDialogFragmentDirections.actionRecordVoiceDialogFragmentToDetailFragment(
+                safeArgs.roomCode,
+                "",
+                true,
+                safeArgs.bookmark
+            )
+        findNavController().navigate(action)
+        findNavController().popBackStack()
+    }
+
+    private fun createComment() {
+        val action =
+            RecordVoiceDialogFragmentDirections.actionRecordVoiceDialogFragmentToDetailFragment(
+                safeArgs.roomCode, "", false, safeArgs.bookmark
+            )
+        findNavController().navigate(action)
+        findNavController().popBackStack()
+    }
+
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
@@ -395,6 +443,7 @@ class RecordVoiceDialogFragment : BottomSheetDialogFragment() {
             MediaRecorderHelper.stopAndRelease()
         }
         if (isPlaying) MediaPlayerHelper.stopAndRelease()
+        keyboardVisibilityUtils.detachKeyboardListeners()
         super.onDestroy()
     }
 
